5f0db85d58e2cae165fd63f539d97edfcfafbd23
DZRABDemo <1156599543@qq.com>
2022-03-13 18:57:28 +0800

    new info

--- src/com/xufeifan/study/basic/TestDemo.java
+++ src/com/xufeifan/study/basic/TestDemo.java
@@ -2,6 +2,18 @@ package com.xufeifan.study.basic;
 
 public class TestDemo {
     public static void main(String[] args) {
-        System.out.println("this is main method");
+        int num =2 ;
+
+        switch(num){
+        case 1:
+            System.out.println("this is one");
+            break;
+        case 2:
+            System.out.println("this is two");
+            break;
+        default:
+            System.out.println("default");
+            break;
+        }
     }
 }
